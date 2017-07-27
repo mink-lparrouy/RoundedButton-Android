c4523de29735e1260f258a9f3b2f49dee5be2b0d
lparrouy <louis.parrouy@mink.so>
2017-07-27 10:42:41 +0200

    Setup library

---
+++ app/src/main/java/com/minkagency/roundedbuttonexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.minkagency.roundedbuttonexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ rounded_button/src/main/java/com/minkagency/roundedbutton/LogDebug.java
@@ -0,0 +1,16 @@
+package com.minkagency.roundedbutton;
+
+/*
+ * Created by louisparrouy on 27/07/2017.
+ */
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "RoundedButton";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+}
